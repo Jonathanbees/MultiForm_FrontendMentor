1902f1b828b3c90c226bf36acb97666dc0d45531
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-06-30 23:21:31 -0500

    challenge advances

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/images/react.svg'
 import './App.css'
+import DesktopSidebar from './assets/images/bg-sidebar-desktop.jpg'
+import { Steps } from './assets/components/steps.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <div className='grid grid-cols-[1fr_3fr] place-items-center bg-slate-400 w-2/3 h-4/6 rounded-xl p-3'>
+      {/* Asegúrate de que este div tenga una altura definida */}
+      <div className={`bg-center bg-[url('./assets/images/bg-sidebar-desktop.svg')] h-full w-full rounded-xl py-10 px-6 `}>
+        <Steps step={'1'} text={'YOUR INFO'} />
+        <Steps step={'2'} text={'SELECT PLAN'} />
+        <Steps step={'3'} text={'ADD-ONS'} />
+        <Steps step={'4'} text={'SUMMARY'} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="menu bg-slate-500">
+        <h1>hola</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
